49950465d8b1fd837c10c55378b0d00ec27e62c6
gembaf <gembaf.github@gmail.com>
2016-06-24 05:35:49 +0000

    不要なファイルは生成しない

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,10 @@ Bundler.require(*Rails.groups)
 
 module Avalon
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
+
